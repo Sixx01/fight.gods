19ccb0ef9779a4c18d9e230eff762b4b47c0651f
Carlos <carlos.arias01@hotmail.com>
2020-02-24 23:30:05 -0600

    Player movement animation
    Player movement horizontal animation, start implementation of jump animation.

--- FightOfGods/Assets/Scripts/PlayerMovement.cs
+++ FightOfGods/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -19,9 +20,42 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           animator.SetInteger("AnimState", 2); 
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetInteger("AnimState", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetInteger("AnimState", 2);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetInteger("AnimState", 0);
+        }
+
+
+
+
+
+
+
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("Grounded", false);
+            animator.SetFloat("AirSpeed", -1);
+            animator.SetInteger("AnimState", 0);
+
+        }
+        else
+        {
+            //animator.SetFloat("AirSpeed", 0);
         }
 
         if (Input.GetButtonDown("Crouch"))
